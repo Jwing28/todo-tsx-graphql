edaa3b97fb475acea9ecee63f247a0494efea644
Jonathan Lee <jonathan.wing.lee@gmail.com>
2021-10-17 21:20:05 -0500

    fix TS errors

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
-interface ATodo {
+export interface ITodo {
   description: string;
   id: number;
 }
 
 const Todo = ({
   todo,
-  onComplete,
+  //   onComplete,
   onDelete,
 }: {
-  todo: ATodo;
-  onComplete: () => {};
-  onDelete: () => {};
+  todo: ITodo;
+  onComplete: (id: number) => void;
+  onDelete: (id: number) => void;
 }) => (
   <div>
-    <div>{todo}</div>
+    <div>{todo.description}</div>
     <div>
-      <button onClick={onComplete}>Completed</button>
-      <button onClick={onDelete}>X</button>
+      {/* <button onClick={() => onComplete(todo.id)}>Completed</button> */}
+      <button onClick={() => onDelete(todo.id)}>X</button>
     </div>
   </div>
 );
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,39 +1,44 @@
 import React, { useState } from 'react';
 import AddTodo from './AddTodo';
-import Todo from './Todo';
+import Todo, { ITodo } from './Todo';
 
-interface TodoId {
-  idx: number;
-}
+interface ITodos extends Array<ITodo> {}
 
 /**
  * Container for todos app.
  * Maintains the state of todos (for now)
  */
 const Todos = () => {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<ITodos>([]);
+  const [todo, setTodo] = useState<ITodo>({ description: '', id: -1 });
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodo(e.target.value);
+    setTodo({ id: 0, description: e.target.value });
   };
 
-  const handleComplete = (idx: TodoId) => {
+  const handleComplete = (id: number) => {
     // todo: have complete and incomplete... later
   };
 
-  const handleDelete = (idx: TodoId) => {
+  const handleDelete = (id: number) => {
     // use id to remove the todo...
+    const updatedTodos: ITodos = todos.filter((todo: ITodo) => todo.id === 0);
+    setTodos(updatedTodos);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // add todo
+    const newTodos: ITodos = todos.slice().concat(todo);
+    setTodos(newTodos);
+  };
 
   <div>
-    <h2>You have ${} todos</h2>
-    <form>
+    <h2>You have ${todos.length} todos</h2>
+    <form onSubmit={handleSubmit}>
       {todos.map((todo, idx) => (
-        <Todo todo={todo} onComplete={} onDelete={} />
+        <Todo todo={todo} onComplete={handleComplete} onDelete={handleDelete} />
       ))}
-      <AddTodo onChange={handleChange} value={todo} />
+      <AddTodo onChange={handleChange} value={todo.description} />
     </form>
   </div>;
 };
